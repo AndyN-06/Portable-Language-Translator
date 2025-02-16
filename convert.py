f7e6ceacfa7f5b4bc74066b72cd23867ea46886d
Yohan Kim <97205472+Yohankim03@users.noreply.github.com>
2025-02-16 17:40:26 -0500

    Update convert.py

--- convert.py
+++ convert.py
@@ -4,4 +4,4 @@ import tensorflow as tf
 model = tf.keras.models.load_model("my_model.keras")
 
 # Convert to SavedModel format
-model.save("my_model_saved", save_format="tf")
+model.save("my_model_saved")
