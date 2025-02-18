866f362582ef1c08e0bfd9af05651c2b19ed56a7
Yohan Kim <yohank@bu.edu>
2025-02-18 17:13:59 -0500

    test

--- convert.py
+++ convert.py
@@ -4,18 +4,25 @@ import tensorflow as tf
 model = tf.keras.models.load_model('my_model.keras')
 
 # 2) Create a TFLiteConverter from the loaded model
-converter = tf.lite.TFLiteConverter.from_saved_model(model)
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
 
-converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS]
-converter.allow_custom_ops = False
-converter.experimental_new_converter = True
-converter.optimizations = [tf.lite.Optimize.DEFAULT]
-# Set input shape explicitly if known
-converter.target_spec.supported_types = [tf.float32]
+# 3) Allow Select TF Ops, so TFLite can handle ops that do not have native TFLite equivalents
+converter.target_spec.supported_ops = [
+    tf.lite.OpsSet.TFLITE_BUILTINS,
+    tf.lite.OpsSet.SELECT_TF_OPS
+]
+
+# 4) Disable the lower_tensor_list_ops so it won't try to convert certain LSTM internals
+converter._experimental_lower_tensor_list_ops = False
+
+# 5) Enable resource variables
+converter.experimental_enable_resource_variables = True
+
+# 6) Convert
 tflite_model = converter.convert()
 
-with open("model.tflite", "wb") as f:
+# 7) Save the .tflite model
+with open('model.tflite', 'wb') as f:
     f.write(tflite_model)
-
 
 print("Conversion successful! Model saved to model.tflite.")
