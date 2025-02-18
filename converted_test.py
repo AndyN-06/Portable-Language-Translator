f547aa805a8071e5bab4d2fa6539b547757e76be
Yohan Kim <yohank@bu.edu>
2025-02-18 17:27:13 -0500

    test

--- converted_test.py
+++ converted_test.py
@@ -83,6 +83,10 @@ cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
+width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+print(f"Current resolution: {int(width)}x{int(height)}")
+
 frame_count = 0
 start_time = time.time()
 
