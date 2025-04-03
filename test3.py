5c71a5e281016441ea9905ac29117068730ca97d
Cristian Palencia <cristian.palencia0211@gmail.com>
2025-04-02 23:43:16 -0400

    added pop up for volume

--- test3.py
+++ test3.py
@@ -1,7 +1,7 @@
 import sys
 import os
 import subprocess
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QPushButton, QSlider
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QPushButton, QSlider, QProgressBar
 from PyQt5.QtCore import Qt, QTimer
 
 
@@ -36,14 +36,10 @@ class VolumeControlApp(QWidget):
         super().__init__()
 
         self.setWindowTitle("Volume Control")
-        self.setGeometry(200, 200, 300, 200)
+        self.setGeometry(200, 200, 300, 250)
 
         # Layout
         self.layout = QVBoxLayout()
-
-        # Volume indicator label
-        self.volume_label = QLabel(f"Current Volume: {get_volume()}%", self)
-        self.layout.addWidget(self.volume_label)
 
         # Volume control slider
         self.volume_slider = QSlider(Qt.Horizontal)
@@ -52,34 +48,38 @@ class VolumeControlApp(QWidget):
         self.volume_slider.valueChanged.connect(self.update_volume_from_slider)
         self.layout.addWidget(self.volume_slider)
 
-        # Increase volume button
+        # Volume control buttons
         self.increase_button = QPushButton("Increase Volume")
         self.increase_button.clicked.connect(lambda: increase_volume(5))
         self.layout.addWidget(self.increase_button)
 
-        # Decrease volume button
         self.decrease_button = QPushButton("Decrease Volume")
         self.decrease_button.clicked.connect(lambda: decrease_volume(5))
         self.layout.addWidget(self.decrease_button)
 
+        # Volume progress bar
+        self.volume_bar = QProgressBar(self)
+        self.volume_bar.setRange(0, 90)
+        self.volume_bar.setValue(get_volume())  # Initial volume level
+        self.layout.addWidget(self.volume_bar)
+
         # Set the layout of the window
         self.setLayout(self.layout)
 
-        # Timer to update the volume label
+        # Timer to periodically update the volume level bar
         self.timer = QTimer(self)
-        self.timer.timeout.connect(self.update_volume_label)
-        self.timer.start(1000)
+        self.timer.timeout.connect(self.update_volume_bar)
+        self.timer.start(1000)  # Update every second
 
     def update_volume_from_slider(self):
         # Get value from the slider and set the volume
         level = self.volume_slider.value()
         set_volume(level)
 
-    def update_volume_label(self):
-        # Update the volume label with the current volume level
+    def update_volume_bar(self):
+        # Update the volume progress bar with the current volume level
         volume = get_volume()
-        self.volume_label.setText(f"Current Volume: {volume}%")
-
+        self.volume_bar.setValue(volume)  # Update the progress bar
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
