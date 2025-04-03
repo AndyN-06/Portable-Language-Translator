a7bf9790fe8bc77caba3fe99e278981169b78ef6
AndyN-06 <andrewvanguyen@icloud.com>
2025-04-03 16:47:22 -0400

    delay timing

--- everything.py
+++ everything.py
@@ -249,8 +249,8 @@ start_time = time.time()
 latest_frame = None
 last_prediction_time = 0
 min_prediction_interval = 0.5
-HISTORY_LENGTH = 5  # Number of predictions to consider
-MIN_CONSISTENT_PREDICTIONS = 4  # Minimum number of same predictions needed
+HISTORY_LENGTH = 4  # Number of predictions to consider
+MIN_CONSISTENT_PREDICTIONS = 3  # Minimum number of same predictions needed
 prediction_history = []  # Store recent predictions
 
 # hands_instance = mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.8)
