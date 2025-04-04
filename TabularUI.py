205d6d177f03f7e96999dddd6b91c53a05b6a9ba
Yohan Kim <yohank@bu.edu>
2025-04-03 20:01:27 -0400

    fix threading

--- TabularUI.py
+++ TabularUI.py
@@ -11,6 +11,7 @@ import os
 from translator_device import TranslatorDevice  # Assuming the device code is in translator_device.py
 import shared
 import threading
+from shared_translator import translator_device
 
 def get_volume():
     result = os.popen("amixer -D pulse get Master").read()
@@ -395,7 +396,7 @@ if __name__ == "__main__":
     with open(file_path, 'w') as file:
         pass  # clear the file contents
 
-    translator_device = TranslatorDevice()
+    #translator_device = TranslatorDevice()
     app = QApplication(sys.argv)
     window = MainWindow(file_path, translator_device)
     window.show()
---
+++ shared_translator.py
@@ -0,0 +1,3 @@
+from translator_device import TranslatorDevice
+
+translator_device = TranslatorDevice()
